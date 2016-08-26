93530e8863a98d11642a8c5a61277ea4ec1ff42f
Kgauza <818683@students.wits.ac.za>
2016-08-26 15:36:53 +0200

    Update process.c
    final

--- hw1/process.c
+++ hw1/process.c
@@ -1,3 +1,11 @@
+/*
+ * process.c
+ *
+ *  Created on: 26 Aug 2016
+ *      Author: vmuser
+ */
+
+
 #include "process.h"
 #include "shell.h"
 #include <errno.h>
@@ -14,6 +22,12 @@
 void launch_process(process *p)
 {
   /** YOUR CODE HERE */
+	SET_SIGNALS(SIG_DFL);
+	dup2(p->stdin, STDIN_FILENO);
+	dup2(p->stdout, STDOUT_FILENO);
+	exec(p->argv[0],p->argv);
+	perror("exec");
+	exit(EXIT_FAILURE);
 }
 
 /**
@@ -25,14 +39,33 @@ void
 put_process_in_foreground (process *p, int cont)
 {
   /** YOUR CODE HERE */
+	if(tcestpgrp(shell_terminal,p->pid)<0)
+		printf("Failed to set the process group of the terminal");
+
+	if(resume){
+		if(!p->completed)
+			p->stopped=false;
+
+		if(kill(-p->pid, SIGCONT)<0)
+			perror("kill (SIGCONT)");
+	}
+
+	wait_for_process(p);
 }
 
 /**
  * Put a process in the background. If the cont argument is true, send
- * the process group a SIGCONT signal to wake it up. 
+ * the process group a SIGCONT signal to wake it up.
  */
 void
 put_process_in_background (process *p, int cont)
 {
   /** YOUR CODE HERE */
+	if(resume){
+		if(!p->completed)
+			p->stopped = false;
+
+		if(kill(-p->pid, SIGCONT)<0)
+			perror("kill (SICONT)");
+	}
 }
