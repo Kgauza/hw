bb3da53316a8beffa5cdc1b508ba6fbad21bf468
Kgauza <818683@students.wits.ac.za>
2016-10-29 00:08:30 +0200

    Update mm_alloc.h

--- mm_alloc.h
+++ mm_alloc.h
@@ -1,14 +1,8 @@
 #include<sys/types.h>
 #include<unistd.h>
 
-/*
- * mm_alloc.h
- *
- * Exports a clone of the interface documented in "man 3 malloc".
- */
 #ifndef __VC_MALLOC__
 #define __VC_MALLOC__
-/* Define the block size since the sizeof will be wrong */
 void *mm_malloc(size_t size);
 void *calloc(size_t number, size_t size);
 void *mm_realloc(void *p, size_t size);
@@ -16,17 +10,16 @@ void *reallocf(void *p, size_t size);
 void  mm_free(void *p);
 
 typedef struct s_block *s_block_ptr;
-/* block struct */
 struct s_block{
     size_t size;
     struct s_block *next;
     struct s_block *prev;
     int free;
     void *ptr;
-    /* A pointer to the allocated block */
+   
     char data[1];
 };
-/* Split block according to size, b must exist */
+ 
 void splis_block_ptr(s_block_ptr b, size_t size);
 
 
